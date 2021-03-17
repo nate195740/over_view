8e51c14ecae9a81bb17d780421ad2458dbb22e2b
nate195740 <nate_piajan@hotmail.com>
2021-03-17 23:55:33 +0700

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname+'/dist/my-overview'));
+app.use(express.static(__dirname + '/dist/over_view'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/my-overview/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/over_view/index.html'))
 });
 
 app.listen(process.env.PORT || 8080);
