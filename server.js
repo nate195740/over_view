e8c88832941e5f10bed1e8092232270034ceaecf
nate195740 <nate_piajan@hotmail.com>
2021-03-18 20:39:14 +0700

    asd

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/my-overview'));
+app.use(express.static(__dirname + '/dist/MY-OVERVIEW'));
 
 app.get('/*',function(req,res){
-    res.send('asdsad');
-    // res.sendFile(path.join(__dirname + '/dist/my-overview/index.html'))
+    // res.send('asdsad');
+    res.sendFile(path.join(__dirname + '/dist/MY-OVERVIEW/index.html'))
 });
 
 app.listen(process.env.PORT || 8080);
